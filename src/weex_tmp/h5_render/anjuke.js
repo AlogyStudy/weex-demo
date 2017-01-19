30d2ed51f415dfcf9cbb08c286bb2f11167d5580
Xingzhang Lin <1129507496@qq.com>
2017-01-19 23:57:33 +0800

    change banner

--- src/weex_tmp/h5_render/anjuke.js
+++ src/weex_tmp/h5_render/anjuke.js
@@ -70,106 +70,169 @@
 	module.exports = {
 	  "type": "div",
 	  "classList": [
-	    "rootview"
+	    "navication"
 	  ],
+	  "style": {
+	    "flexDirection": "row",
+	    "height": 100
+	  },
 	  "children": [
 	    {
 	      "type": "div",
-	      "classList": [
-	        "navication"
-	      ],
 	      "style": {
-	        "flexDirection": "row",
-	        "height": 100,
-	        "background": "yellowgreen"
+	        "flex": 0.4,
+	        "justifyContent": "center",
+	        "alignItems": "center"
+	      },
+	      "children": [
+	        {
+	          "type": "text",
+	          "style": {
+	            "color": "#ffffff",
+	            "fontSize": 32
+	          },
+	          "attr": {
+	            "value": "上海 >"
+	          }
+	        }
+	      ]
+	    },
+	    {
+	      "type": "div",
+	      "style": {
+	        "flex": 1,
+	        "justifyContent": "center"
 	      },
 	      "children": [
 	        {
 	          "type": "div",
 	          "style": {
-	            "flex": 0.4,
+	            "flexDirection": "row",
+	            "backgroundColor": "#f0f7e6",
 	            "justifyContent": "center",
-	            "alignItems": "center"
+	            "alignItems": "center",
+	            "borderRadius": 4,
+	            "marginLeft": 20,
+	            "marginRight": 20,
+	            "height": 50,
+	            "lineHeight": 50,
+	            "fontSize": 14
+	          },
+	          "attr": {
+	            "value": "// 图标文字"
 	          },
 	          "children": [
+	            {
+	              "type": "image",
+	              "attr": {
+	                "src": function () {return this.searchIcon}
+	              },
+	              "style": {
+	                "width": 44,
+	                "height": 44
+	              }
+	            },
 	            {
 	              "type": "text",
-	              "attr": {
-	                "value": "上海 >"
-	              }
-	            }
-	          ]
-	        },
-	        {
-	          "type": "div",
-	          "style": {
-	            "flex": 1,
-	            "background": "tan",
-	            "justifyContent": "center"
-	          },
-	          "children": [
-	            {
-	              "type": "div",
 	              "style": {
-	                "flexDirection": "row",
-	                "background": "#fff",
-	                "justifyContent": "center",
-	                "alignItems": "center",
-	                "borderRadius": 4,
-	                "marginLeft": 20,
-	                "marginRight": 20,
-	                "height": 70
+	                "color": "#999999"
 	              },
 	              "attr": {
-	                "value": "// 图标文字"
+	                "value": "挑好房,上安居客"
 	              },
 	              "children": [
 	                {
-	                  "type": "image",
+	                  "type": "input",
 	                  "attr": {
-	                    "src": ""
-	                  },
-	                  "style": {
-	                    "width": 44,
-	                    "height": 44,
-	                    "background": "cyan"
-	                  }
-	                },
-	                {
-	                  "type": "text",
-	                  "attr": {
-	                    "value": "搜索房源"
-	                  }
-	                },
-	                {
-	                  "type": "image",
-	                  "style": {
-	                    "width": 44,
-	                    "height": 44,
-	                    "background": "cyan"
+	                    "type": "text",
+	                    "placeholder": "挑好房,上安居客"
 	                  }
 	                }
 	              ]
 	            }
 	          ]
-	        },
+	        }
+	      ]
+	    },
+	    {
+	      "type": "div",
+	      "style": {
+	        "flex": 0.4,
+	        "flexDirection": "row",
+	        "justifyContent": "center",
+	        "alignItems": "center"
+	      },
+	      "children": [
 	        {
-	          "type": "div",
-	          "style": {
-	            "flex": 0.4,
-	            "background": "pink",
-	            "flexDirection": "row",
-	            "justifyContent": "center",
-	            "alignItems": "center"
+	          "type": "a",
+	          "attr": {
+	            "href": "#"
 	          },
 	          "children": [
 	            {
-	              "type": "image"
-	            },
-	            {
-	              "type": "text",
+	              "type": "image",
 	              "attr": {
-	                "value": "地图"
+	                "src": function () {return this.address}
+	              },
+	              "style": {
+	                "width": 14,
+	                "height": 18,
+	                "backgroundSize": 397,
+	                "verticalAlign": "middle"
+	              }
+	            }
+	          ]
+	        },
+	        {
+	          "type": "a",
+	          "attr": {
+	            "href": "#"
+	          },
+	          "children": [
+	            {
+	              "type": "imgage",
+	              "attr": {
+	                "src": function () {return this.address}
+	              },
+	              "style": {
+	                "width": 15,
+	                "height": 18,
+	                "WebkitBackgroundSize": "",
+	                "backgroundSize": 397,
+	                "verticalAlign": "middle"
+	              }
+	            }
+	          ]
+	        }
+	      ]
+	    },
+	    {
+	      "type": "slider",
+	      "classList": [
+	        "slider"
+	      ],
+	      "attr": {
+	        "interval": "3000",
+	        "autoPlay": "true"
+	      },
+	      "children": [
+	        {
+	          "type": "div",
+	          "classList": [
+	            "frame"
+	          ],
+	          "attr": {
+	            "vFor": "img in imageList"
+	          },
+	          "children": [
+	            {
+	              "type": "image",
+	              "classList": [
+	                "image"
+	              ],
+	              "attr": {
+	                "resize": "cover",
+	                ":src": "img.src"
 	              }
 	            }
 	          ]
@@ -183,17 +246,47 @@
 /* 2 */
 /***/ function(module, exports) {
 
-	module.exports = {}
+	module.exports = {
+	  "navication": {
+	    "backgroundColor": "#62ab00"
+	  },
+	  "image": {
+	    "width": 700,
+	    "height": 700
+	  },
+	  "slider": {
+	    "marginTop": 25,
+	    "marginLeft": 25,
+	    "width": 700,
+	    "height": 700,
+	    "borderWidth": 2,
+	    "borderStyle": "solid",
+	    "borderColor": "#41B883"
+	  },
+	  "frame": {
+	    "width": 700,
+	    "height": 700,
+	    "position": "relative"
+	  }
+	}
 
 /***/ },
 /* 3 */
 /***/ function(module, exports) {
 
-	module.exports = function(module, exports, __weex_require__){"use strict";
+	module.exports = function(module, exports, __weex_require__){'use strict';
 
 	module.exports = {
-	        data: function () {return {}},
-	        methods: {}
+	    data: function data() {
+	        return {
+	            searchIcon: '',
+	            address: 'http://pages.anjukestatic.com/usersite/touch/img/home/home_img3.png',
+	            imageList: [{ src: 'https://gd2.alicdn.com/bao/uploaded/i2/T14H1LFwBcXXXXXXXX_!!0-item_pic.jpg' }, { src: 'https://gd1.alicdn.com/bao/uploaded/i1/TB1PXJCJFXXXXciXFXXXXXXXXXX_!!0-item_pic.jpg' }, { src: 'https://gd3.alicdn.com/bao/uploaded/i3/TB1x6hYLXXXXXazXVXXXXXXXXXX_!!0-item_pic.jpg' }]
+	        };
+	    },
+	    methods: {},
+	    created: function created() {},
+	    ready: function ready() {}
 	};}
 	/* generated by weex-loader */
 
